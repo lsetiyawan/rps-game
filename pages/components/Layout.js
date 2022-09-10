feb82f4ac40cffa0d3f5e02409a0f34e1576b9de
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-09-10 21:48:33 +0700

    project core

---
+++ pages/components/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "../../styles/components/Layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <div>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <Link href="/register">
+              <a class="active">Register</a>
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/login">
+              <a>Login</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ pages/game/[id]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameId = () => {
+  return (
+    <div>GameId</div>
+  )
+}
+
+export default GameId
